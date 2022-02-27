48ea070159c985d1b0db7922cf22e0e34ebd9f22
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2022-02-27 07:35:25 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (5.99%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int temp;
+        while(b != 0)
+        {
+            temp = (a & b) << 1;
+            a = a ^ b;
+            b = temp;
+        }
+        
+        return a;
+    }
+}
