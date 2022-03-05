9c859735c15627dd852c73c03977627ed46732fb
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2022-03-05 20:30:17 +0530

    Time: 34 ms (76.70%), Space: 13.9 MB (57.79%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,24 +1,35 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        #check = {')' : '(', ']' : '[', '}' : '{'}
-        if len(s) == 1: 
-            return False
-        if s[0] == ')' or s[0] == '}' or s[0] == ']':
-            return False
-        for c in s:
-            if c == '(' or c == '{' or c == '[':
-                stack.append(c)
-            elif c == ')' and stack and stack[-1] == '(':
-                stack.pop()
-            elif c == '}' and stack and stack[-1] == '{':
-                stack.pop()
-            elif c == ']' and stack and stack[-1] == '[':
-                stack.pop()
-            else:
-                return False 
+#         stack = []
+
+#         if len(s) == 1: 
+#             return False
+#         if s[0] == ')' or s[0] == '}' or s[0] == ']':
+#             return False
+#         for c in s:
+#             if c == '(' or c == '{' or c == '[':
+#                 stack.append(c)
+#             elif c == ')' and stack and stack[-1] == '(':
+#                 stack.pop()
+#             elif c == '}' and stack and stack[-1] == '{':
+#                 stack.pop()
+#             elif c == ']' and stack and stack[-1] == '[':
+#                 stack.pop()
+#             else:
+#                 return False 
         
+#         return len(stack) == 0
+          
+        stack = []
+        check = {'(' : ')', '[' : ']', '{' : '}'}
+        for i in s:
+            if i in check:
+                stack.append(i)
+            elif not stack or check[stack.pop()] != i:
+                return False
+                
         return len(stack) == 0
+        
             
             
         
