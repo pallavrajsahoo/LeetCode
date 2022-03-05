b0d2de54a07157337e0f2cf94d754d86e0ccca4e
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2022-03-05 20:35:05 +0530

    Time: 61 ms (15.35%), Space: 13.9 MB (85.48%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,35 +1,35 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-#         stack = []
-
-#         if len(s) == 1: 
-#             return False
-#         if s[0] == ')' or s[0] == '}' or s[0] == ']':
-#             return False
-#         for c in s:
-#             if c == '(' or c == '{' or c == '[':
-#                 stack.append(c)
-#             elif c == ')' and stack and stack[-1] == '(':
-#                 stack.pop()
-#             elif c == '}' and stack and stack[-1] == '{':
-#                 stack.pop()
-#             elif c == ']' and stack and stack[-1] == '[':
-#                 stack.pop()
-#             else:
-#                 return False 
-        
-#         return len(stack) == 0
-          
         stack = []
-        check = {'(' : ')', '[' : ']', '{' : '}'}
-        for i in s:
-            
-            if i in check:
-                stack.append(i)
-            elif len(stack) == 0 or check[stack.pop()] != i:
-                return False
-                
+
+        if len(s) == 1: 
+            return False
+        if s[0] == ')' or s[0] == '}' or s[0] == ']':
+            return False
+        for c in s:
+            if c == '(' or c == '{' or c == '[':
+                stack.append(c)
+            elif c == ')' and stack and stack[-1] == '(':
+                stack.pop()
+            elif c == '}' and stack and stack[-1] == '{':
+                stack.pop()
+            elif c == ']' and stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                return False 
+        
         return len(stack) == 0
+          
+#         stack = []
+#         check = {'(' : ')', '[' : ']', '{' : '}'}
+#         for i in s:
+            
+#             if i in check:
+#                 stack.append(i)
+#             elif len(stack) == 0 or check[stack.pop()] != i:
+#                 return False
+                
+#         return len(stack) == 0
         
             
             
