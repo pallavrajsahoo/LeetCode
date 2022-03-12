9cd44de7493258ba2bb028e87b68fa1d5f6b33cd
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2022-03-12 18:05:46 +0530

    Time: 56 ms (99.97%), Space: 14 MB (52.31%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,25 +1,45 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
+        if s == s[::-1]:
+            return s
+#         n = len(s)
+#         start = 0
+#         max_length = 0
+        
+#         for current in range(len(s)):
+#             right = current + 1
+#             left = current - 1
+            
+#             while right < n and s[right] == s[current]:
+#                 right += 1
+#             while left >= 0 and s[left] == s[current]:
+#                 left -= 1
+#             while right < n and left >= 0 and s[left] == s[right]:
+#                 left -= 1
+#                 right += 1
+            
+#             current_length = right - left - 1
+            
+#             if(max_length < current_length):
+#                 max_length = current_length
+#                 start = left + 1
+        
+#         return s[start : start+max_length]
+        
         start = 0
-        max_length = 0
-        
-        for current in range(len(s)):
+        length = 1
+        for current in range(1, len(s)):
+            left = current - length
             right = current + 1
-            left = current - 1
             
-            while right < n and s[right] == s[current]:
-                right += 1
-            while left >= 0 and s[left] == s[current]:
-                left -= 1
-            while right < n and left >= 0 and s[left] == s[right]:
-                left -= 1
-                right += 1
+            s1 = s[left-1 : right]
+            s2 = s[left : right]
             
-            current_length = right - left - 1
+            if s1 == s1[::-1] and left - 1 >= 0:
+                start = left - 1
+                length += 2
+            elif s2 == s2[::-1]:
+                start = left
+                length += 1
             
-            if(max_length < current_length):
-                max_length = current_length
-                start = left + 1
-        
-        return s[start : start+max_length]
+        return s[start : start+length]
