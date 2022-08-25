761c7f864db55584c6f801e8f0bd275cfc14d173
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2022-08-25 15:15:39 -0400

    Time: 114 ms (31.76%), Space: 14.2 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -2,6 +2,7 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         mag_cnt = Counter(magazine)
         ran_cnt = Counter(ransomNote)
+        
         return ran_cnt & mag_cnt == ran_cnt
         
         
