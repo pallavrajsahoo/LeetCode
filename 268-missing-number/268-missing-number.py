d14935d2e15d51afbe8ab14d6ca2e97abb623d67
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2022-08-29 16:09:21 -0400

    Time: 248 ms (37.63%), Space: 15.2 MB (79.01%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -2,4 +2,5 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         max_sum = sum([i for i in range(n+1)])
+        
         return max_sum - sum(nums)
