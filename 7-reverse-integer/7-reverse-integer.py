6f165aacc393ec71e24d93635ce7dfee893da2cb
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2022-09-28 16:27:59 -0400

    Time: 48 ms (67.62%), Space: 13.9 MB (16.54%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -3,11 +3,12 @@ class Solution:
         result = 0
         sign = -1 if x<0 else 1
         x = abs(x)
+        
         while x:
             digit = x%10
             result = digit + 10 * result
             x = x//10
-       
+     
         if result > 2 ** 31-1 or x < -2 ** 31:
             return 0
         
