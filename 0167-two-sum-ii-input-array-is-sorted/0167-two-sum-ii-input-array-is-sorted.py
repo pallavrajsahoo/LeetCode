abdedb8b0451399a6b91c3bdb46a4dd65ac7a127
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2023-05-30 17:28:08 -0400

    Time: 143 ms (33.12%), Space: 17.2 MB (34.13%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -3,11 +3,11 @@ class Solution:
         left = 0
         right = len(numbers)-1
 
-        while(left < right):
+        while numbers[left] + numbers[right] != target:
             total = numbers[left] + numbers[right]
             if total < target:
                 left += 1
-            elif total > target:
+            else :
                 right -= 1
-            else:
-                 return [left+1, right+1]
+            
+        return [left+1, right+1]
