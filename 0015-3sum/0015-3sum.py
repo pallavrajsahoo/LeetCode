293f4eb4fafc3fba0528f1de460999e378079be0
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2023-06-14 12:27:37 -0400

    Time: 1504 ms (55.82%), Space: 20.5 MB (39.24%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,10 +6,9 @@ class Solution:
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
             left = i+1
             right = len(nums) - 1
-            
+
             while left < right:
                 three_sum = nums[i] + nums[left] + nums[right]
                 if three_sum > 0:
