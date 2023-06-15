f0408577980832a86e2200d410f75d10299cc5c4
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2023-06-15 13:36:46 -0400

    Time: 50 ms (34.34%), Space: 16.4 MB (41.34%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracket = {"}":"{", ")":"(", "]":"["}
+        stack=[]
+
+        for ch in s:
+            if ch in bracket:
+                if stack and stack[-1] == bracket[ch]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(ch)
+            
+        return len(stack) == 0
+
+        
