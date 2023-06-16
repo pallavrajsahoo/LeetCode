403c5a28d433835aa8e151b83e05a6554a2924e0
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2023-06-16 12:04:12 -0400

    Time: 119 ms (48.02%), Space: 21.2 MB (53.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = {}
-        result = []
         for n in nums:
             if n in freq:
                 freq[n] += 1
