706cf5d77a7426fdb42abf6647d798adbd9a9ac8
pallavraj sahoo <33052397+pallavrajsahoo@users.noreply.github.com>
2023-06-22 17:09:13 -0400

    Time: 104 ms (25.47%), Space: 16.5 MB (41.35%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,7 +8,6 @@ class Solution:
             while s[right] in char_set:
                 char_set.remove(s[left])
                 left += 1
-            
             char_set.append(s[right])
             length = max(length, len(char_set))
 
